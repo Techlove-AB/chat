86cab32e6ac98744a2ccc9c3161abae004d40681
Tommie Lagerroos <tommie@lagerroos.nu>
2022-07-04 17:06:23 +0200

    Added cacheable to base model

--- src/BaseModel.php
+++ src/BaseModel.php
@@ -3,8 +3,11 @@
 namespace Musonza\Chat;
 
 use Illuminate\Database\Eloquent\Model;
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 
 class BaseModel extends Model
 {
+    use Cachable;
+    protected $cachePrefix = "vfore";
     protected $tablePrefix = 'chat_';
 }
